bcc9eee0e825c5f1ef3dcc7bcf8399d3b6648c2d
cau2005 <francescocau2005@gmail.com>
2023-09-27 16:43:34 +0200

    Create Spring Project

---
+++ cloudolderpc/src/main/java/com/cloud/cloudolderpc/CloudolderpcApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.cloudolderpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudolderpcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudolderpcApplication.class, args);
+	}
+
+}
